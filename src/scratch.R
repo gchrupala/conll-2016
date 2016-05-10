704882781a0a7e3a57d27a7636b02603c5c1369d
Grzegorz Chrupała <pitekus@gmail.com>
2016-05-10 18:24:21 +0200

    src

--- src/scratch.R
+++ src/scratch.R
@@ -1,10 +1,12 @@
 library(dplyr)
 library(ggplot2)
-data <- read.table("data.txt", col.names = c("epoch","item","x","split","loss","exp"))
+library(stringr)
+data <- read.table("data.txt", col.names = c("epoch","item","x","split","loss","exp")) %>% 
+  mutate(kind = str_sub(exp, -4))
 MAX <- max(data$item)
-ggplot(data=data, aes(x=(epoch-1) * MAX + item, y=loss, color=exp)) + 
-  geom_point(size=2, alpha=0.3) + 
-  geom_line() + 
+ggplot(data=data, aes(x=(epoch-1) * MAX + item, y=loss, color=kind)) + 
+  geom_point(alpha=0.3) + 
+  #geom_line() + 
   geom_vline(xintercept = (data$epoch-1) * MAX, color='gray') +
   ylim(c(0.48,0.6)) + 
   xlab("t") 
