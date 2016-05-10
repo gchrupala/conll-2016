7f3b491943247912aaae565fd67eae698f6cedee
Grzegorz Chrupała <pitekus@gmail.com>
2016-05-10 18:14:43 +0200

    src

--- src/scratch.R
+++ src/scratch.R
@@ -5,5 +5,6 @@ MAX <- max(data$item)
 ggplot(data=data, aes(x=(epoch-1) * MAX + item, y=loss, color=exp)) + 
   geom_point(size=2, alpha=0.3) + 
   geom_line() + 
+  geom_vline(xintercept = (data$epoch-1) * MAX, color='gray') +
   ylim(c(0.48,0.6)) + 
-  xlab("t")
+  xlab("t") 
