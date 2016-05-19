ec1ea883537c3413e9fa2a0cb7a234c59a8ab6e7
Grzegorz Chrupała <pitekus@gmail.com>
2016-05-19 13:42:25 +0200

    Changed style file to EMNLP 2016

--- src/scratch.R
+++ src/scratch.R
@@ -4,9 +4,10 @@ library(stringr)
 data <- read.table("data.txt", col.names = c("epoch","item","x","split","loss","exp")) %>% 
   mutate(kind = str_sub(exp, -4))
 MAX <- max(data$item)
-ggplot(data=data, aes(x=(epoch-1) * MAX + item, y=loss, color=kind)) + 
-  geom_point(alpha=0.3) + 
-  #geom_line() + 
+ggplot(data=data, aes(x=(epoch-1) * MAX + item, y=loss, color=exp)) + 
+  geom_point(alpha=0.2) + 
+  geom_line() + 
   geom_vline(xintercept = (data$epoch-1) * MAX, color='gray') +
   ylim(c(0.48,0.6)) + 
+  #xlim(c(0.0, 2*MAX)) +
   xlab("t") 
